71612ddd7b43af9a612745e903fadb1c84e1fb77
Adil Aslam <aaliyanaslam22@gmail.com>
2026-04-16 09:49:59 +0500

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // tumhara config (same)
 const firebaseConfig = {
-  apiKey: "AIzaSyAB9bobX9AnEa7h0hYYV6l6NW3BoEC0yT4",
-  authDomain: "creator-tools-b72e8.firebaseapp.com",
-  projectId: "creator-tools-b72e8",
-  storageBucket: "creator-tools-b72e8.firebasestorage.app",
-  messagingSenderId: "406944808193",
-  appId: "1:406944808193:web:c6ab192ec048758d29af00",
-  measurementId: "G-MCKPFE7T79"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // ✅ safe initialize (important in Next.js)
